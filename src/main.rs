df22dd25042295e6521cc076999b5fff688e9442
ONOUE Origami <arclisp@plotdot.net>
2024-06-28 23:41:22 +0900

    feat: wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-mod credentials_file;
+use std::fmt::Debug;
 
-use clap::{Parser};
+use clap::Parser;
+use strum::IntoEnumIterator;
+
+mod credentials_file;
+mod fields;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,8 +19,13 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("{}", args.profile);
-    println!("{}", args.mfa_device.is_some());
+    let profile = args.profile;
+    let profile_long_term = format!("{}-long-term", profile.clone());
     let file = credentials_file::read_aws_credentials_ini();
-    println!("{}", file.len())
+    let section = file.section(Some(profile_long_term)).expect("this profile is not found");
+    let aws_key_long_term = section.get(fields::LongTermFields::AwsAccessKeyId.to_string()).expect("aws_access_key_id is not found");
+    let aws_secret_long_term = section.get(fields::LongTermFields::AwsSecretAccessKey.to_string()).expect("aws_secret_access_key is not found");
+
+    println!("{}", aws_key_long_term);
+    println!("{}", aws_secret_long_term);
 }
