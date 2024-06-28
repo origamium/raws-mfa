81618164e1ede9beb1636fedf3388acacd0f0068
ONOUE Origami <arclisp@plotdot.net>
2024-06-28 20:42:45 +0900

    feat: mainを定義

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-mod file;
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // optional
+    #[arg(short = 'm', long)]
+    mfa_device: Option<String>,
+
+    #[arg(short = 'p', long, default_value = "default")]
+    profile: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{}", args.profile);
+    println!("{}", args.mfa_device.is_some())
 }
